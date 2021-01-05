f3925aa0af4af9f95ed80e909f0d2ffccdde3d2c
itokoji <gyiteng473@gmail.com>
2021-01-05 17:00:06 +0900

    バリデーションの記述修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  with_options presence: true do
+    validates :nickname
+    validates :encrypted_password, length: { minimum: 6 }
 
-  validates :nickname,               presence: true
-  validates :encrypted_password,     presence: true, length: { minimum: 6 }
-  validates :last_name,              presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name,             presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :last_name_kana,         presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana,        presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
-  validates :birth_date,             presence: true
+    with_options format: {with: /\A[ぁ-んァ-ン一-龥]/ } do
+      validates :last_name
+      validates :first_name
+
+    with_options format: {with: /\A[ァ-ヶー－]+\z/ } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+
+    validates :birth_date
+  end
 end
