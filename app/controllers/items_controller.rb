40ec5e181402043107493a6d0811b61b430e90c9
itokoji <gyiteng473@gmail.com>
2021-01-07 12:39:27 +0900

    商品出品ページへ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,13 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :item_info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delovery_id, :item_price)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one :order
+  
+  with_options presence: true do
+    validates :item_name
+    validates :item_info
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validates :scheduled_delovery_id
+    validates :item_price
+  end
 end
